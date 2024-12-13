4cef274765e4c15574bc50fadbc666e6ed427d25
Eyobel11 <154692239+Eyobel11@users.noreply.github.com>
2024-12-13 16:56:47 +0300

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -7,12 +7,12 @@ require("dotenv").config();
 const vehicleRoutes = require("./routes/vehicelRoutes");
 
 const app = express();
-// app.use(cors());
+app.use(cors());
 
-app.use(cors({
-  origin: 'mini-proj-vehicle-managment-uzl2.vercel.app', // Frontend URL
-  methods: ['GET', 'POST', 'PUT', 'DELETE'],
-}));
+// app.use(cors({
+//   origin: 'mini-proj-vehicle-managment-uzl2.vercel.app', // Frontend URL
+//   methods: ['GET', 'POST', 'PUT', 'DELETE'],
+// }));
 
 app.use(bodyParser.json());
 
